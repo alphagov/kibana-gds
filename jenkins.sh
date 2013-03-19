81e12c3aaf5899de922dbc7edbf2c6ee67b867d6
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-03-19 10:42:02 +0000

    Actually bundle quietly

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eu
 
-bundle install -q --path "${HOME}/bundles/${JOB_NAME}" --deployment
+bundle install --quiet --path "${HOME}/bundles/${JOB_NAME}" --deployment
 exec bundle exec ruby test/test_authwrapper.rb
